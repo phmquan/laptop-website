0c1c9df329fba8a56fc3e0ebf61e2d271f9e3c04
phmquan <phanhoangminhquan240402@gmail.com>
2024-06-25 11:25:46 +0700

    initial structure for project

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -1,13 +1,19 @@
 package vn.hoidanit.laptopshop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		ApplicationContext QuanPhanPrint = SpringApplication.run(LaptopshopApplication.class, args);
+		for(String s: QuanPhanPrint.getBeanDefinitionNames())
+		{
+			System.out.println(s);
+		}
 	}
 
 }
